587aba2537fec08b77a26884b928b326677b9401
Daniel McGrath <daniel@venodesigns.net>
2011-06-24 14:08:17 -0400

    It's all so hacky

--- roulette/views.py
+++ roulette/views.py
@@ -11,6 +11,28 @@ def index(request):
 	}, context_instance = RequestContext(request))
 	
 def meetup(request):
+	# It's easier to keep tweaking the filter rules if it's ugly like this
+	def is_possible_meetup(meetup):
+		if "fee" in meetup and meetup['fee']['required'] == 1:
+			return False
+		elif meetup['group']['join_mode'] != 'open':
+			return False
+		elif "rsvp_limit" in meetup and meetup['yes_rsvp_count'] >= meetup['rsvp_limit']:
+			return False
+		elif "venue" not in meetup:
+			return False
+		else:
+			return True
+			
+	def group_requires_info(group_id):
+		url = "https://api.meetup.com/2/groups?key=%s&sign=true&fields=join_info&group_id=%s" % (settings.MEETUP_API_KEY, group_id)
+		data = json.loads(urllib2.urlopen(url).read().decode('latin1').encode('utf8'))
+		res = data['results'][0]
+		if res['join_info']['questions_req'] == "0" and res['join_info']['intro_req'] == "0":
+			return False
+		else:
+			return res['join_info']
+	
 	if request.method == 'POST':
 		form = RandomMeetupForm(request.POST)
 		if form.is_valid():
@@ -29,16 +51,28 @@ def meetup(request):
 
 			while is_okay_to_rsvp == False:
 				meetup = random.choice(meetups['results'])
-				if "fee" not in meetup and meetup['group']['join_mode'] == 'open':
-					if "rsvp_limit" in meetup and meetup['yes_rsvp_count'] >= meetup['rsvp_limit']:
-						is_okay_to_rsvp = False
-					else:
- 						is_okay_to_rsvp = True
+				is_okay_to_rsvp = is_possible_meetup(meetup)
 			
+			group_join_requirements = group_requires_info("1781190")
+			if group_join_requirements:
+				if "questions" in group_join_requirements:
+					question_fields = group_join_requirements['questions']
+				else:
+					question_fields = ()
+					
+				if group_join_requirements['intro_req'] == "1":
+					intro_fields = True
+				else:
+					intro_fields = False
+			else:
+				question_fields = ()
+				intro_fields = False
 			
 			return render_to_response('roulette/meetup.html', {
 				'meetup': meetup,
-				'form': new_form
+				'form': new_form,
+				'intro_fields': intro_fields,
+				'question_fields': question_fields
 			}, context_instance = RequestContext(request))
 	else:
 		return HttpResponseRedirect('/')
